c215ffb1c44b81208db4902682b14cd97314da74
zero <mariobigboy@gmail.com>
2020-01-30 19:41:05 -0300

    products.blade

---
+++ resources/views/products/products.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width-device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Products</title>
+    </head>
+    <body>
+        <div class="container">
+
+            <h1>Products</h1>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card-header">
+                        Products List
+                        <a href="#" class="btn btn-success btn-sm float-right">New Product</a>
+                    </div>
+                    <div class="card-body">
+                        
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+    </body>
+
+</html>
